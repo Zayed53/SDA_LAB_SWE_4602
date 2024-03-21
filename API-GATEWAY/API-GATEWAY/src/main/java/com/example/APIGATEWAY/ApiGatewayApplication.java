ee6a407bf8a8e60c82ae3dc6d7be175683cc0291
Zayed Hasan <95176649+Zayed53@users.noreply.github.com>
2024-03-22 04:11:31 +0600

    Add files via upload

---
+++ API-GATEWAY/API-GATEWAY/src/main/java/com/example/APIGATEWAY/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.APIGATEWAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
+
+//            uri: http://localhost:9002/employees/ (in application.yml)
