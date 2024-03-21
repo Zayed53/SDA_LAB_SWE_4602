e8545b86c6642cd39467c4c178b766f14cf42852
Zayed Hasan <95176649+Zayed53@users.noreply.github.com>
2024-03-22 04:09:32 +0600

    Delete Order-Service/Order-Service directory

--- Order-Service/Order-Service/src/main/java/com/example/OrderService/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.OrderService.repository;
-
-import com.example.OrderService.entity.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderRepository extends MongoRepository<Order, String> {
-    Order findOrderById(String Id);
-}
